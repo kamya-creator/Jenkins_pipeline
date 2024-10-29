a784d500aa5409f8e362fae1df3f1658520477cf
kamya-creator <kamyap2000@gmail.com>
2024-10-29 14:04:30 +0530

    file

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Main{
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello Krishna");
+        System.out.println("Date is " + new Date());
+    }
+}
