6521cca48522c5901e6a3e62d08bd2df6c259ab3
kamya-creator <kamyap2000@gmail.com>
2024-10-29 14:31:33 +0530

    Student file added

--- Main.java
+++ Main.java
@@ -7,5 +7,8 @@ class Main{
         System.out.println("Hello Krishna");
         System.out.println("Date is " + new Date());
         System.out.println("Added new line");
+
+        Student student = new Student();
+        student.sayHi();
     }
 }
---
+++ Student.java
@@ -0,0 +1,12 @@
+class Student{
+
+    Student()
+    {
+        System.out.println("This is student object");
+    }
+
+    public void sayHi()
+    {
+        System.out.println("Say Hello to student");
+    }
+}
